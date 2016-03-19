f397e5c57756b4191b71789c27a81fd0f63bd497
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2016-03-19 14:14:12 -0700

    initial commit

---
+++ client/scripts/library.js
@@ -0,0 +1,13 @@
+var library = [
+  {
+    artist: "BTS",
+    title: "Dope",
+    img_url: "http://1.soompi.io/wp-content/uploads/2015/06/BTS-2.jpg"
+  },
+  {
+    artist: "Exo",
+    title: "Call Me Baby",
+    img_url: "http://1.soompi.io/wp-content/uploads/2015/08/SPAO-Lookbook8.png"
+  },
+
+]
